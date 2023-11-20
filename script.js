71f2e8c6c71660ef4f2bde8b4b8829f510f3fbee
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2023-11-20 12:41:09 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let responseData;
+
+document.addEventListener("DOMContentLoaded", fetchData);
+
+async function fetchData() {
+  const apiUrl = "https://api.kanye.rest";
+
+  try {
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    responseData = await response.json();
+
+    console.log("Data from API: ", responseData);
+    displayData();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+function displayData() {
+  const resultDiv = document.getElementById("result");
+  resultDiv.innerHTML = "";
+
+  if (!responseData || !responseData.quote) {
+    resultDiv.innerHTML = "<p>No quote available</p>";
+    return;
+  }
+
+  const quoteDiv = document.createElement("div");
+  quoteDiv.classList.add("quote");
+
+  const content = `<p>${responseData.quote}</p>`;
+  quoteDiv.innerHTML = content;
+
+  resultDiv.appendChild(quoteDiv);
+}
+
+displayData();
